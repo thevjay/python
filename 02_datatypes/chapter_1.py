df58b7432c82905cbda3a21e5b150fa09a3a2334
thevjay <rebalvijay930@gmail.com>
2025-09-17 22:33:05 +0530

    mutable and immutable

--- 02_datatypes/chapter_1.py
+++ 02_datatypes/chapter_1.py
@@ -1,2 +1,13 @@
-sugar_amount=2
+sugar_amount = 2
 print(f"Initial sugar: {sugar_amount}")
+
+sugar_amount = 12
+print(f"Updated sugar: {sugar_amount}")
+
+print(f"ID of 2: {id(2)}")
+print(f"ID of 12: {id(12)}")
+
+# Initial sugar: 2
+# Updated sugar: 12
+# ID of 2: 140724163896136
+# ID of 12: 140724163896456
---
+++ 02_datatypes/chapter_2.py
@@ -0,0 +1,9 @@
+spice_mix = set()
+print(f"Inintial spice mix id: {id(spice_mix)}")
+
+spice_mix.add("cumin")
+spice_mix.add("turmeric")
+print(f"Spice mix after adding cumin and turmeric: {id(spice_mix)}")
+
+# Inintial spice mix id: 2265770531456
+# Spice mix after adding cumin and turmeric: 2265770531456
